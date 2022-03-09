614db9c3f00e0d33d705985f7973e12efc3c5ca9
Paul Solbach <p@psolbach.com>
2022-03-09 08:52:26 +0100

    2022 analysis

---
+++ tools/convert-api-follower-data.py
@@ -0,0 +1,86 @@
+# import pdb; pdb.set_trace()
+
+import csv
+import json
+
+from os import listdir
+from os.path import isfile, join
+
+CSV_PATH_E = "csv/graphdata_e.csv"
+CSV_PATH_V = "csv/graphdata_v.csv"
+USERDATA_PATH = "userdata/"
+
+def string_to_hash(string, length=8):
+  return abs(hash(string)) % (10 ** length)
+
+def get_graph_data(path, filter_common=True):
+  vertex_counts = {}
+  vertices = []
+  edges = []
+
+  files = [f for f in listdir(path) if isfile(join(path, f))]
+
+  for fname in files:
+    username = fname.split(".")[0]
+    vertices.append([username.lower()]) # append original node
+    
+    if fname[0] == ".": continue
+    f = open(USERDATA_PATH + fname, 'r') 
+    
+    try:
+      following = json.load(f)
+    except:
+      print(f"Error reading file {fname}")
+    
+    for user in following:
+      follower = fname.split(".")[0].lower()
+      followee = user["username"].lower()
+
+      print(f"User {follower} follows {followee}")
+
+      if not vertex_counts.get(followee):
+        vertex_counts[followee] = 0
+
+      vertex_counts[followee] += 1
+
+      if vertex_counts.get(followee) > 1:
+        vertices.append([followee]) # append follow node
+
+      edges.append([
+        string_to_hash(follower),
+        string_to_hash(followee),
+        follower,
+        followee
+        ]) # append edge values
+
+  # only write edges with degree >1
+  common_edges = []
+  for edge in edges:
+    if vertex_counts.get(edge[3]) > 1:
+      common_edges.append(edge)
+
+  for vertex in vertices:
+    vertex.append(vertex_counts.get(vertex[0])) # add weight
+
+  return {
+    "v": vertices,
+    "e": common_edges if filter_common else edges
+  }
+
+def write_to_csv(data, csv_path): 
+  print("Writing graph data to .csv file")
+
+  with open(csv_path, 'w+', newline='') as csvfile:
+    graphwriter = csv.writer(csvfile, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+
+    for i, line in enumerate(data):
+      graphwriter.writerow(data[i])
+
+  return True
+
+graph_data = get_graph_data(path=USERDATA_PATH)
+write_result_v = write_to_csv(data=graph_data["v"], csv_path=CSV_PATH_V)
+write_result_e = write_to_csv(data=graph_data["e"], csv_path=CSV_PATH_E)
+
+print("Finished writing {} vertices with message {}".format(len(graph_data["v"]), write_result_v))
+print("Finished writing {} edges with message {}".format(len(graph_data["e"]), write_result_e))
---
+++ tools/get-follows-twarc.py
@@ -0,0 +1,39 @@
+import os
+import json
+import time
+
+from twarc.client2 import Twarc2
+from twarc.expansions import ensure_flattened
+from users import users
+
+# Your bearer token here
+t = Twarc2(bearer_token=os.environ['TWITTER_BEARER_TOKEN'])
+
+for i, user in enumerate(users):
+  print(f"Fetching follows {i+1}/{len(users)} with username {user}")
+
+  username = user.lower()
+  filepath = f"userdata/{username}.json"
+
+  if not os.path.exists(filepath):
+    with open(filepath, 'w+'): pass
+
+  if os.path.getsize(filepath) > 0:
+    continue
+  
+  try:
+    follows = []
+    follows_generator = t.following(user=username)
+    
+    for i, follower_page in enumerate(follows_generator):
+      print(f"Processing page {i+1} for {username}")
+      follows = follows + follower_page["data"]
+
+    with open(filepath, 'w+') as f:
+      f.write(json.dumps(follows))
+
+  except e:
+    print(f"Collecting follows for {username} failed with {e}")
+
+  print(f"Sleeping for 8 minutes to avoid throttling")
+  time.sleep(60*8)
